95658b39ac31898fb26ebf53cb24d38c9c380ac9
Sourav Bhunia <souravbhunia162@gmail.com>
2024-06-27 21:58:46 +0530

    Call For Paper, Accepted Paper Page added

---
+++ src/api/accepted-paper-page/controllers/accepted-paper-page.ts
@@ -0,0 +1,7 @@
+/**
+ * accepted-paper-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::accepted-paper-page.accepted-paper-page');
---
+++ src/api/call-for-paper-page/controllers/call-for-paper-page.ts
@@ -0,0 +1,7 @@
+/**
+ * call-for-paper-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::call-for-paper-page.call-for-paper-page');
