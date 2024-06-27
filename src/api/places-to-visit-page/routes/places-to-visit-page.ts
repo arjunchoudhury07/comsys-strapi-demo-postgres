326f475d787f87b6367a8d8e552362f2d230ebf0
Sourav Bhunia <souravbhunia162@gmail.com>
2024-06-27 23:59:00 +0530

    Place to visit and Gallery bug fixed

---
+++ src/api/places-to-visit-page/routes/places-to-visit-page.ts
@@ -0,0 +1,7 @@
+/**
+ * places-to-visit-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::places-to-visit-page.places-to-visit-page');
