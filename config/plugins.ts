c4094131a9e641a396d5407f97714ca8d95c4f04
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2025-07-01 19:43:58 +0530

    added email plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,4 +3,16 @@ export default () => ({
     enabled: true,
     resolve: "strapi-bulk-operator",
   },
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend",
+      providerOptions: {
+        apiKey: process.env["RESEND_API_KEY"], // Required
+      },
+      settings: {
+        defaultFrom: "comsyseducationaltrust@resend.dev", // env('RESEND_DEFAULT_FROM')
+        defaultReplyTo: "me@example.com", // env('RESEND_DEFAULT_FROM')
+      },
+    },
+  },
 });
